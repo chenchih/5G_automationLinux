7a8a88d59c4246e19191c76f281c229cb3d0124e
ChenChih <jacklee26@gmail.com>
2023-06-02 17:42:37 +0800

    add new feature drawLineGraph contain Line graph data visualization

---
+++ log_graph/FinalCode/Layer2/drawLineGraph/chart_TPUT_only.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read the Excel file into a pandas DataFrame
+data = pd.read_excel('2.xlsx')
+
+# Extract datetime and Tput columns
+datetime = data['datetime']
+tput = data['Tput']
+
+# Convert the datetime column to numeric values using NumPy
+x = np.arange(len(datetime))
+
+
+#plt.figure(figsize=(20, 6.5), dpi=300)
+#####change 202306
+plt.figure(figsize=(15,5), dpi=300)
+
+
+
+
+# Plot the line graph
+plt.plot(datetime, tput, label='DL')
+
+
+plt.xlabel('time')
+plt.ylabel('Tput')
+plt.title('TPUT')
+
+#tick_interval = 10  # Adjust the interval as per your preference
+#plt.xticks(range(0, len(datetime), tick_interval), rotation=90)
+#######202306 chnage
+plt.xticks( np.linspace(0, len(datetime)-1, 100 ),rotation=90 )
+
+
+#plt.xticks(rotation=90, fontsize=12)
+
+plt.legend()
+#adjust the layout 
+plt.tight_layout() 
+#plt.show()
+
+plt.savefig('tput.png')
